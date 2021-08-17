13d45557fcf22ab87b612863b8d81350ad984165
CoderMahabub <codermahabub@gmail.com>
2021-08-18 02:16:25 +0600

    Initial Calculation of deposit and withdraw done using JS

--- js/banking.js
+++ js/banking.js
@@ -1,4 +1,46 @@
-//
+//Handle Deposit Money
 document.getElementById('deposit-button').addEventListener('click', function () {
+    //get event handler button
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText);
+    // console.log(depositAmount);
 
+    //get current deposit
+    const depositTotal = document.getElementById('deposit-total');
+    const depositTotalText = depositTotal.innerText;
+    const previousDepositTotal = parseFloat(depositTotalText);
+    depositTotal.innerText = previousDepositTotal + depositAmount;
+
+    //update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    balanceTotal.innerText = previousBalanceTotal + depositAmount;
+
+    //clear input field
+    depositInput.value = '';
+});
+
+//Handle Withdraw Money
+document.getElementById('withdraw-button').addEventListener('click', function () {
+
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawInput.value;
+    const withdrawAmount = parseFloat(withdrawAmountText);
+
+
+    //get withdraw Total 
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const withdrawTotalText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(withdrawTotalText);
+    withdrawTotal.innerText = previousWithdrawTotal + withdrawAmount;
+
+    //Update Balance after withdraw
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    balanceTotal.innerText = previousBalanceTotal - withdrawAmount;
+
+    withdrawInput.value = '';
 });
